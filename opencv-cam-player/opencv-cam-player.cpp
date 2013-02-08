381b2e9d40480ef4bc979dfbd9937cccd5cd6f7d
mr.Shu <mr@shu.io>
2013-02-08 16:06:46 +0100

    first commit

---
+++ opencv-cam-player/opencv-cam-player.cpp
@@ -0,0 +1,45 @@
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+using namespace cv;
+
+
+
+int main( int argc, const char** argv )
+{
+    CvCapture* capture = 0;
+    Mat frame, frameCopy, image;
+
+    capture = cvCaptureFromCAM(0);
+    if(!capture) cout << "No camera detected" << endl;
+    cvNamedWindow( "result", 1 );
+
+
+    if(capture) {
+        cout << "We've got a camera!" << endl;
+        while (1) {
+            IplImage* iplImg = cvQueryFrame( capture );
+            frame = iplImg;
+            if(frame.empty())
+                break;
+
+            if(iplImg->origin == IPL_ORIGIN_TL)
+                frame.copyTo(frameCopy);
+            else
+                flip(frame, frameCopy, 0);
+
+            imshow("result", frame);
+            waitKey(20);
+        }
+    }
+    cvDestroyWindow("result");
+
+    return 0;
+}
