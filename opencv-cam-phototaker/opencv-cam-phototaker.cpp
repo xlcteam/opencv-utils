62205290d23409834ce12f2926105f6e96085536
mr.Shu <mr@shu.io>
2013-02-08 16:40:37 +0100

    added phototaker

---
+++ opencv-cam-phototaker/opencv-cam-phototaker.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using namespace std;
+using namespace cv;
+
+
+
+int main(int argc, const char** argv)
+{
+    CvCapture* capture = 0;
+    Mat frame, frameCopy, image;
+    int cam = (argc > 1 ? atoi(argv[1]) : 0);
+
+    capture = cvCaptureFromCAM(cam);
+    if(!capture) cout << "No camera detected" << endl;
+    cvNamedWindow( "result", 1 );
+
+
+    if(capture) {
+        cout << "We've got a camera!" << endl;
+        while (1) {
+            IplImage* iplImg = cvQueryFrame( capture );
+            frame = iplImg;
+            if(frame.empty())
+                break;
+
+            if(iplImg->origin == IPL_ORIGIN_TL)
+                frame.copyTo(frameCopy);
+            else
+                flip(frame, frameCopy, 0);
+
+            imshow("result", frame);
+            int c = waitKey(20);
+            if (c == 112) {
+                char filename[100];
+                sprintf(filename, "photo-%ld.jpg", time(0));
+                imwrite(filename, frame);
+                cout << "Saved frame to image " << filename << endl;
+            }
+        }
+    }
+    cvDestroyWindow("result");
+
+    return 0;
+}
